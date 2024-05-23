ade630e9f74251a92baa0fbf72af96f44678834d
buian-dragos <dragos.buian@stud.ubbcluj.ro>
2024-05-23 13:59:42 +0300

    pagina cu carde

---
+++ academia-innova/src/app/user/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+import React from "react";
+import { Label } from "../components/ui/label";
+import { Input } from "../components/ui/input";
+import { CategoryCards } from "../components/ui/cards";
+import { cn } from "../utils/cn";
+import { IconBrandGithub, IconBrandGoogle, IconBrandOnlyfans } from "@tabler/icons-react";
+
+const SignupFormDemo = () => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Form submitted");
+  };
+  return (
+    <div>
+        <CategoryCards/>
+    </div>
+  );
+};
+
+export default SignupFormDemo;
+
+const BottomGradient = () => {
+  return (
+    <>
+      <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
+      <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
+    </>
+  );
+};
+
+const LabelInputContainer = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div className={cn("flex flex-col space-y-2 w-full", className)}>
+      {children}
+    </div>
+  );
+};
